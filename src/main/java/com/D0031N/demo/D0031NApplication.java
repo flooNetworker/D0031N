8841bc967e91f876ed219486ff4b855367e6d8cf
Mimmi Sandström <mimmi.sandstroem@gmail.com>
2025-11-04 11:05:16 +0100

    Add dependencies and the spring boot project

---
+++ src/main/java/com/D0031N/demo/D0031NApplication.java
@@ -0,0 +1,14 @@
+package com.D0031N.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D0031NApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		SpringApplication.run(D0031NApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/D0031N/demo/D0031NApplicationTests.java
@@ -0,0 +1,13 @@
+package com.D0031N.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D0031NApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
